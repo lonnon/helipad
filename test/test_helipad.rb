a052485007bdcae1b00ab8e02a7bfa8673cdac46
Lonnon Foster <lonnon.foster@gmail.com>
2008-12-23 13:26:35 -0800

    Tests rewritten for get, get_html, update. Also fixed assertions wrapped inside assert_nothing_raised blocks; assertions contained within those blocks didn't report properly, which makes sense now that I think about how Test::Unit must work. Yanked out the rest of the test code that requires REXML, too; all the XML parsing happens inside the Helipad class now, so there's no need to manually parse stuff out in the tests. Only test left right now is test_get_all, and for that, I'll need to fully implement the get_all method of Helipad.

--- test/test_helipad.rb
+++ test/test_helipad.rb
@@ -2,7 +2,6 @@ $:.unshift File.join(File.dirname(__FILE__), "..", "lib")
 require 'helipad'
 require 'test/unit'
 require 'rexml/document'
-include REXML
 
 class TestHelipad < Test::Unit::TestCase
   def setup
@@ -12,32 +11,33 @@ class TestHelipad < Test::Unit::TestCase
   end
   
   def test_create
+    response = nil
     assert_nothing_raised do
       response = @hp.create(:title => "New document", :tags => "test",
                             :source => "Just a test document")
-      assert_equal(true, response.doc_saved?, "Document not created.")
-      @hp.destroy(response.doc_id) if response.doc_saved?
     end
+    assert_equal(true, response.doc_saved?, "Document not created.")
+    @hp.destroy(response.doc_id) if response.doc_saved?
   end
   
   def test_destroy
     create_response = @hp.create(:title => "Document for deletion", :tags => "test",
                                  :source => "Should be deleted by test case")
+    response = nil
     assert_nothing_raised do
       response = @hp.destroy(create_response.doc_id)
-      assert_equal(true, response.doc_deleted?, "Document not deleted.")
     end
+    assert_equal(true, response.doc_deleted?, "Document not deleted.")
   end
 
   def test_get
+    doc = nil
     assert_nothing_raised do
-      @hp.get(1)
+      doc = @hp.get(1)
     end
-    doc = Document.new @hp.get(1)
-    assert_equal("test", doc.root.elements["title"].text, "Title is wrong.")
-    assert_equal("test", doc.root.elements["source"].text, "Source is wrong.")
-    assert_equal("test", doc.root.elements["tags/tag/name"].text, "Tags are wrong.")
-    assert_equal(1, XPath.match(doc, "//tag").size, "Wrong number of tags.")
+    assert_equal("test", doc.doc_title, "Title is wrong.")
+    assert_equal("test", doc.doc_source, "Source is wrong.")
+    assert_equal(["test"], doc.doc_tags, "Tags are wrong.")
   end
   
   def test_get_all
@@ -47,39 +47,41 @@ class TestHelipad < Test::Unit::TestCase
   end
   
   def test_get_html
+    html = nil
     assert_nothing_raised do
-      doc = Document.new @hp.get_html(1)
+      html = @hp.get_html(1)
     end
+    exemplar = %{<h1>test</h1>
+<p>test</p>}
+    assert_equal(exemplar, html, "HTML contents are wrong.")
   end
 
   def test_update
     create_response = @hp.create(:title => "Document to be modified", :tags => "test",
                                  :source => "Modify me, baby")
     if create_response.doc_saved?
+      response = nil
       assert_nothing_raised do
         response = @hp.update(create_response.doc_id)
-        assert_nil(response, "Update should return nil when no params are supplied.")
-
-        response = @hp.update(create_response.doc_id, :title => "Modified the title")
-        assert_equal(true, response.doc_saved?, "Document wasn't updated.")
-        doc = Document.new @hp.get(create_response.doc_id)
-        assert_equal("Modified the title", doc.root.elements["title"].text, "Title is wrong.")
-        
-        response = @hp.update(create_response.doc_id, :source => "Modified, darlin'")
-        assert_equal(true, response.doc_saved?, "Document wasn't updated.")
-        doc = Document.new @hp.get(create_response.doc_id)
-        assert_equal("Modified, darlin'", doc.root.elements["source"].text, "Source is wrong.")
-
-        response = @hp.update(create_response.doc_id, :tags => "test stuff")
-        assert_equal(true, response.doc_saved?, "Document wasn't updated.")
-        doc = Document.new @hp.get(create_response.doc_id)
-        tags = Array.new
-        XPath.match(doc, "//tags/tag/name/child::text()").each do |tag|
-          tags.push tag.to_s
-        end
-        assert_equal(["test", "stuff"], tags, "Tags are wrong.")
       end
-      @hp.destroy(id)
+      assert_nil(response, "Update should return nil when no params are supplied.")
+
+      response = @hp.update(create_response.doc_id, :title => "Modified the title")
+      assert_equal(true, response.doc_saved?, "Document title wasn't updated.")
+      doc = @hp.get(create_response.doc_id)
+      assert_equal("Modified the title", doc.doc_title, "Title is wrong.")
+      
+      response = @hp.update(create_response.doc_id, :source => "Modified, darlin'")
+      assert_equal(true, response.doc_saved?, "Document source wasn't updated.")
+      doc = @hp.get(create_response.doc_id)
+      assert_equal("Modified, darlin'", doc.doc_source, "Source is wrong.")
+
+      response = @hp.update(create_response.doc_id, :tags => "test stuff")
+      assert_equal(true, response.doc_saved?, "Document tags weren't updated.")
+      doc = @hp.get(create_response.doc_id)
+      assert_equal(["test", "stuff"], doc.doc_tags, "Tags are wrong.")
+
+      @hp.destroy(create_response.doc_id)
     end
   end
 end
