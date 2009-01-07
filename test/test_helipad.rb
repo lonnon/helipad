75918d88f8f556092e490ca85276119dbecf5799
Lonnon Foster <lonnon.foster@gmail.com>
2009-01-06 16:41:13 -0800

    Reworked unit tests to create and destroy a test record for each test. No need for hardcoded test document in ID 1 any more.

--- test/test_helipad.rb
+++ test/test_helipad.rb
@@ -9,6 +9,15 @@ class TestHelipad < Test::Unit::TestCase
     @email = ARGV[0]
     @password = ARGV[1]
     @hp = Helipad.new(@email, @password)
+    @test_string = [Array.new(6){rand(256).chr}.join].pack("m").chomp
+    response = @hp.create(:title => "test #{@test_string}",
+                          :tags => "test #{@test_string}", 
+                          :source => "test #{@test_string}")
+    @test_id = response.doc_id
+  end
+  
+  def teardown
+    @hp.destroy(@test_id)
   end
   
   def test_create
@@ -34,17 +43,19 @@ class TestHelipad < Test::Unit::TestCase
   def test_find
     documents = nil
     assert_nothing_raised do
-      documents = @hp.find("test")
+      documents = @hp.find(@test_string)
     end
-    assert_equal("test", documents[0].title, "By search term: First document title is wrong.")
+    assert_equal("test #{@test_string}", documents[0].title,
+                 "By search term: First document title is wrong.")
     
     documents = @hp.find("32908p9832tn9p85h92gng825g82ngp88p9834p98v348anvs8")
     assert_nil(documents, "Bogus string should not be found.")
     
     assert_nothing_raised do
-      documents = @hp.find(:tag, "test")
+      documents = @hp.find(:tag, @test_string)
     end
-    assert_equal("test", documents[0].title, "By tag: First document title is wrong.")
+    assert_equal("test #{@test_string}", documents[0].title,
+                 "By tag: First document title is wrong.")
     
     documents = @hp.find(:tag, "02vhn45g70h4cgh0872h54gmv072hm45g70hmv0hmv2g20457gh")
     assert_nil(documents, "Bogus tag should not be found.")
@@ -57,11 +68,11 @@ class TestHelipad < Test::Unit::TestCase
   def test_get
     doc = nil
     assert_nothing_raised do
-      doc = @hp.get(1)
+      doc = @hp.get(@test_id)
     end
-    assert_equal("test", doc.title, "Title is wrong.")
-    assert_equal("test", doc.source, "Source is wrong.")
-    assert_equal(["test"], doc.tags, "Tags are wrong.")
+    assert_equal("test #{@test_string}", doc.title, "Title is wrong.")
+    assert_equal("test #{@test_string}", doc.source, "Source is wrong.")
+    assert_equal(["test", @test_string], doc.tags, "Tags are wrong.")
   end
   
   def test_get_all
@@ -69,18 +80,15 @@ class TestHelipad < Test::Unit::TestCase
     assert_nothing_raised do
       documents = @hp.get_all
     end
-    assert_equal("test", documents[0].title, "First document title is wrong.")
-    assert_equal("test", documents[0].source, "First document source is wrong.")
-    assert_equal(["test"], documents[0].tags, "First document tags are wrong.")
   end
   
   def test_get_html
     html = nil
     assert_nothing_raised do
-      html = @hp.get_html(1)
+      html = @hp.get_html(@test_id)
     end
-    exemplar = %{<h1>test</h1>
-<p>test</p>}
+    exemplar = %{<h1>test #{@test_string}</h1>
+<p>test #{@test_string}</p>}
     assert_equal(exemplar, html, "HTML contents are wrong.")
   end
 
@@ -89,33 +97,26 @@ class TestHelipad < Test::Unit::TestCase
     assert_nothing_raised do
       documents = @hp.get_titles
     end
-    assert_equal("test", documents[0].title, "First document title is wrong.")
   end
   
   def test_update
-    create_response = @hp.create(:title => "Document to be modified", :tags => "test",
-                                 :source => "Modify me, baby")
-    if create_response.saved?
-      response = nil
-      assert_raise(ArgumentError) {@hp.update(create_response.doc_id)}
-      assert_raise(ArgumentError) {@hp.update(create_response.doc_id, :wibble => "freem")}
-      
-      response = @hp.update(create_response.doc_id, :title => "Modified the title")
-      assert_equal(true, response.saved?, "Document title wasn't updated.")
-      doc = @hp.get(create_response.doc_id)
-      assert_equal("Modified the title", doc.title, "Title is wrong.")
-      
-      response = @hp.update(create_response.doc_id, :source => "Modified, darlin'")
-      assert_equal(true, response.saved?, "Document source wasn't updated.")
-      doc = @hp.get(create_response.doc_id)
-      assert_equal("Modified, darlin'", doc.source, "Source is wrong.")
-
-      response = @hp.update(create_response.doc_id, :tags => "test stuff")
-      assert_equal(true, response.saved?, "Document tags weren't updated.")
-      doc = @hp.get(create_response.doc_id)
-      assert_equal(["test", "stuff"], doc.tags, "Tags are wrong.")
-
-      @hp.destroy(create_response.doc_id)
-    end
+    response = nil
+    assert_raise(ArgumentError) {@hp.update(@test_id)}
+    assert_raise(ArgumentError) {@hp.update(@test_id, :wibble => "freem")}
+    
+    response = @hp.update(@test_id, :title => "Modified the title")
+    assert_equal(true, response.saved?, "Document title wasn't updated.")
+    doc = @hp.get(@test_id)
+    assert_equal("Modified the title", doc.title, "Title is wrong.")
+    
+    response = @hp.update(@test_id, :source => "Modified, darlin'")
+    assert_equal(true, response.saved?, "Document source wasn't updated.")
+    doc = @hp.get(@test_id)
+    assert_equal("Modified, darlin'", doc.source, "Source is wrong.")
+    
+    response = @hp.update(@test_id, :tags => "test stuff")
+    assert_equal(true, response.saved?, "Document tags weren't updated.")
+    doc = @hp.get(@test_id)
+    assert_equal(["test", "stuff"], doc.tags, "Tags are wrong.")
   end
 end
